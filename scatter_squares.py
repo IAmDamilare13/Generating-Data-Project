c2e01eb76a15e9612e6e125f08ff52a88ba1a206
Damilare Daramola <86056544+IAmDamilare13@users.noreply.github.com>
2025-01-21 06:35:34 +0100

    Add files via upload

---
+++ scatter_squares.py
@@ -0,0 +1,26 @@
+# Calcualting Data Automatically
+
+import matplotlib.pyplot as plt
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+# plt.style.use('seaborn')
+fig, ax = plt.subplots()
+# ax.scatter(x_values, y_values, color=(0, 0.8, 0), s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10) # Colormap
+
+# Set chart title and label axes
+ax.set_title("Sqaure Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+# Set size of tick label
+ax.tick_params(labelsize=14)
+
+# Set the range for each axis
+ax.axis([0, 1100, 1, 1_100_000])
+ax.ticklabel_format(style='plain')
+
+# plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
